3feddd5616ab6f09c18e3292b93c7514f7077642
Gorka Fernandez <gfernandez@codesyntax.eus>
2022-10-10 17:35:52 +0200

    added reducers

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+export const addFaqItem = (question, answer) => ({
+	type: "ADD_FAQ_ITEM",
+	question,
+	answer,
+});
+export const editFaqItem = (index, question, answer) => ({
+	type: "EDIT_FAQ_ITEM",
+	index,
+	question,
+	answer,
+});
+
+export const deleteFaqItem = (index) => ({
+	type: "DELETE_FAQ_ITEM",
+	index,
+});
