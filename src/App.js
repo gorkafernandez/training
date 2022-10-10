0961fed95b36f879e9e1f7bf6c7ea7ffad8235ea
Gorka Fernandez <gfernandez@codesyntax.eus>
2022-10-10 15:28:28 +0200

    add funcionalities

--- src/App.js
+++ src/App.js
@@ -1,30 +1,76 @@
+import { useState } from "react";
 import "./App.css";
 import FaqItem from "./components/FaqItem/FaqItem";
 
 function App() {
+	const [faqList, setFaqList] = useState([
+		{
+			question: "What does the Plone Foundation do?",
+			answer: "The mission of the Plone Foundation is to protect and...",
+		},
+		{
+			question: "Why does Plone need a Foundation?",
+			answer: "Plone has reached critical mass, with enterprise...",
+		},
+	]);
+	const onDelete = (index) => {
+		let faq = [...faqList];
+		faq.splice(index, 1);
+		setFaqList(faq);
+	};
+	const [question, setQuestion] = useState();
+	const [answer, setAnswer] = useState();
+	const onChangeQuestion = (e) => {
+		setQuestion(e.target.value);
+		console.log(question);
+	};
+	const onChangeAnswer = (e) => {
+		setAnswer(e.target.value);
+		console.log(answer);
+	};
+	const onEdit = (index, question, answer) => {
+		const faq = [...faqList];
+		faq[index] = { question, answer };
+		setFaqList(faq);
+	};
+
+	const onSubmitForm = (e) => {
+		e.preventDefault();
+		setFaqList([...faqList, { question, answer }]);
+		setQuestion("");
+		setAnswer("");
+	};
+
 	return (
 		<div className="App">
 			<ul>
-				<FaqItem
-					question="Question 1: What does the Plone Foundation do?"
-					answer="Answer 1: The mission of the Plone Foundation is to protect and
-						promote Plone. The Foundation provides marketing assistance,
-						awareness, and evangelism assistance to the Plone community. The
-						Foundation also assists with development funding and coordination of
-						funding for large feature implementations. In this way, our role is
-						similar to the role of the Apache Software Foundation and its
-						relationship with the Apache Project."
-				/>
-				<FaqItem
-					question="Question 2: Why does Plone need a Foundation?"
-					answer="Answer 2: Plone has reached critical mass, with enterprise
-						implementations and worldwide usage. The Foundation is able to speak
-						for Plone, and provide strong and consistent advocacy for both the
-						project and the community. The Plone Foundation also helps ensure a
-						level playing field, to preserve what is good about Plone as new
-						participants arrive."
-				/>
+				{faqList.map((item, index) => (
+					<FaqItem
+						key={index}
+						question={item.question}
+						answer={item.answer}
+						index={index}
+						onDelete={onDelete}
+						onEdit={onEdit}
+					/>
+				))}
 			</ul>
+			<form onSubmit={onSubmitForm}>
+				<label>
+					Question:{" "}
+					<input
+						name="question"
+						type="text"
+						value={question}
+						onChange={onChangeQuestion}
+					/>
+				</label>
+				<label>
+					Answer:{" "}
+					<textarea name="answer" value={answer} onChange={onChangeAnswer} />
+				</label>
+				<input type="submit" value="Add" />
+			</form>
 		</div>
 	);
 }
--- src/components/FaqItem/FaqItem.jsx
+++ src/components/FaqItem/FaqItem.jsx
@@ -1,18 +1,81 @@
 import "./FaqItem.css";
 import PropTypes from "prop-types";
 
+import { useState } from "react";
+
 const FaqItem = (props) => {
+	const [show, setShow] = useState(false);
+	const [edit, setEdit] = useState(false);
+	const [question, setQuestion] = useState("");
+	const [answer, setQuestionAnswer] = useState("");
+
+	const toggle = () => {
+		setShow(!show);
+	};
+	const onDelete = () => {
+		console.log("button Clicked");
+		props.onDelete(props.index);
+	};
+	const onEdit = () => {
+		setEdit(true);
+		setQuestionAnswer(props.answer);
+		setQuestion(props.question);
+	};
+	const onChangeAnswer = (e) => {
+		setQuestionAnswer(e.target.value);
+	};
+	const onChangeQuestion = (e) => {
+		setQuestion(e.target.value);
+	};
+
+	const onSave = (e) => {
+		e.preventDefault();
+		setEdit(false);
+		props.onEdit(props.index, question, answer);
+	};
 	return (
-		<li className="faq-item">
-			<h2 className="question">{props.question}</h2>
-			<p>{props.answer}</p>
-		</li>
+		<>
+			{edit ? (
+				<li className="faq-item">
+					<form onSubmit={onSave}>
+						<label>
+							Question:
+							<input
+								name="question"
+								value={question}
+								onChange={onChangeQuestion}
+							/>
+						</label>
+						<label>
+							Answer:
+							<textarea
+								name="answer"
+								value={answer}
+								onChange={onChangeAnswer}
+							/>
+						</label>
+						<input type="submit" value="Save" />
+					</form>
+				</li>
+			) : (
+				<li className="faq-item">
+					<h2 className="question" onClick={toggle}>
+						{props.question}
+					</h2>
+					{show && <p>{props.answer}</p>}
+					<button onClick={onDelete}>Delete</button>
+					<button onClick={onEdit}>Edit</button>
+				</li>
+			)}
+		</>
 	);
 };
-
 FaqItem.propTypes = {
 	question: PropTypes.string.isRequired,
 	answer: PropTypes.string.isRequired,
+	index: PropTypes.number.isRequired,
+	onDelete: PropTypes.func.isRequired,
+	onEdit: PropTypes.func.isRequired,
 };
 
 export default FaqItem;
